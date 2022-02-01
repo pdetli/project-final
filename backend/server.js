a13260c31c6a439e2c045884c144be5716f73ece
pdetli <74938586+pdetli@users.noreply.github.com>
2022-02-01 15:15:28 +0100

    categories and server updated

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
-import express from 'express'
-import data from './data.js'
-import cors from 'cors'
+import express from "express"
+import data from "./data.js"
+import cors from "cors"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/products', (req, res) => {
-    res.json(data.products)
+app.get("/api/products", (req, res) => {
+  res.json(data.products)
 })
-
 
 // get a list of products by querying their genre (from json file)
 app.get("/api/products/genre", (req, res) => {
@@ -18,7 +17,7 @@ app.get("/api/products/genre", (req, res) => {
   let vinylsGenreData = data.products
 
   if (genre) {
-    // api/products/genre?genre=pop  
+    // api/products/genre?genre=pop
     vinylsGenreData = vinylsGenreData.filter(
       (item) => item.genre.toLowerCase().indexOf(genre.toLowerCase()) !== -1
     )
@@ -26,25 +25,22 @@ app.get("/api/products/genre", (req, res) => {
   res.json(vinylsGenreData)
 })
 
+app.get("/api/products/id/:id", async (req, res) => {
+  const { id } = req.params
+  const idNr = await data.products.find((item) => item._id === id)
 
-app.get('/api/products/id/:id', async (req, res) => {
-    const { id } = req.params
-    const idNr = await data.products.find(item => item._id === id)
-
-    if (!idNr) {  
-      res.status(404).send('No product by that Id')
-    } else {
-      res.json(idNr)
-    }
-  })
-
-  
-app.get('/', (req, res) => {
-    res.send('Server is ready!!!!')
+  if (!idNr) {
+    res.status(404).send("No product by that Id")
+  } else {
+    res.json(idNr)
+  }
 })
 
+app.get("/", (req, res) => {
+  res.send("Server is ready!!!!")
+})
 
 const port = process.env.PORT || 3003
 app.listen(3003, () => {
-    console.log(`Server at http://localhost:${port}`)
+  console.log(`Server at http://localhost:${port}`)
 })
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,73 @@
-import React, {useEffect} from 'react'
-import Product from '../components/Product'
-import Loading from '../components/Loading' 
-import GenreCard from '../components/GenreCard'
+import React, { useEffect, useState } from "react"
+import Product from "../components/Product"
+//import Loading from "../components/Loading"
 
 import { showShop } from "../reducers/shop"
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux"
 
 const HomeScreen = () => {
-    const dispatch = useDispatch()
-    const products = useSelector((store) => store.shop.items)
-    const loading = useSelector((store) => store.shop.loading)
-   
-    useEffect(() => {
-        dispatch(showShop())
-    }, [dispatch])
+  const dispatch = useDispatch()
+  const products = useSelector((store) => store.shop.items)
+  // const loading = useSelector((store) => store.shop.loading)
+  const [selectedGenre, setSelectedGenre] = useState([])
 
-    return (
-        <main>
-            <GenreCard />
-            {loading ? (
-               <Loading/> 
-            ) : (
-                <div className="row center">       
-                    {products.map(product => (
-                        <Product key={product._id} product={product} />
-                    ))}
-                </div>  
-            )} 
-        </main>
-    )
+  useEffect(() => {
+    dispatch(showShop())
+  }, [dispatch])
+
+  const handleInput = (props) => {
+    fetch(`http://localhost:3003/api/products/${props}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("selected genre", data)
+        setSelectedGenre(data)
+      })
+  }
+
+  return (
+    <main>
+      <div className="genre-container">
+        <button type="image" onClick={() => handleInput("")}>
+          <img src="/assets/all.png" height="100" width="100" />
+          <p> GET'EM ALL</p>
+        </button>
+        <button type="image" onClick={() => handleInput("genre/?genre=pop")}>
+          <img src="/assets/pop.png" height="100" width="100" />
+          <p> POP</p>
+        </button>
+        <button type="button" onClick={() => handleInput("genre/?genre=hip")}>
+          <img src="/assets/hiphop.png" height="100" width="100" />
+          <p> HIP HOP</p>
+        </button>
+        <button type="button" onClick={() => handleInput("genre/?genre=rock")}>
+          <img src="/assets/rock.png" height="100" width="100" />
+          <p> ROCK</p>
+        </button>
+        <button
+          type="button"
+          onClick={() => handleInput("genre/?genre=electronic")}
+        >
+          <img src="/assets/electronic.png" height="100" width="100" />
+          <p> ELECTRONIC</p>
+        </button>
+      </div>
+
+      {selectedGenre.length === 0 && (
+        <section className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </section>
+      )}
+      {selectedGenre.length !== 0 && (
+        <section className="row center">
+          {selectedGenre.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </section>
+      )}
+    </main>
+  )
 }
 
-export default HomeScreen    
+export default HomeScreen
